c7f66b8f97a0eab659b8c597fa16f99876a166e3
Pablo de Jesus <jesusvazquez2302@gmail.com>
2023-02-03 20:33:41 -0600

    Rest client: suma de dos numeros

---
+++ server.js
@@ -0,0 +1,44 @@
+/* const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const sayHi = (req, res) => {
+  res.send("Hi!");
+};
+
+app.get("/", sayHi);
+
+app.post("/add", (req, res) => {
+  const { a, b } = req.body;
+  res.send(`The sum is: ${a + b}`);
+});
+
+app.listen(5000, () => {
+  console.log(`Server is running on port 5000.`);
+});
+ */
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/add", (req, res) => {
+  const { a, b } = req.body;
+  res.send({
+    result: parseInt(a) + parseInt(b),
+  });
+});
+
+app.listen(5000, () => {
+  console.log(`Server is running on port 5000.`);
+});
